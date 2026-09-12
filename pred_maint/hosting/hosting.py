9bc3d02bf4f27e60d7d6603db23d24111952735c
maitrayeeb-cmyk <maitrayeeb@gmail.com>
2026-09-12 20:34:46 +0530

    Update hosting.py

--- pred_maint/hosting/hosting.py
+++ pred_maint/hosting/hosting.py
@@ -4,7 +4,7 @@ import os
 api = HfApi(token=os.getenv("HF_TOKEN"))
 api.upload_folder(
     folder_path="pred_maint/deployment",     # the local folder containing my files
-    repo_id = "mrhea/Predictive-Maintenance-Capstone"              # the target repo
+    repo_id = "mrhea/Predictive-Maintenance-Capstone",              # the target repo
     repo_type="space",                      # dataset, model, or space
     path_in_repo="",                          # optional: subfolder path inside the repo
 )
